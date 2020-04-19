a8565e9d7d9e141d4515c0d92987b4619dca24d3
Georgei Goncharik <georgii.goncharik@gmail.com>
2020-04-20 00:07:18 +0300

    bug fix

--- bsuirapp/views.py
+++ bsuirapp/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def get_lol_page(request):
-    return render(request, 'static/lolpage.html')
+    return render(request, 'lolpage.html')
